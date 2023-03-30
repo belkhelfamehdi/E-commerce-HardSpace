381efe03bd725d755b62bd7537c8d50fbe556b41
SparkyDZZ <124880239+SparkyDZZ@users.noreply.github.com>
2023-03-30 23:12:30 +0100

    Init commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +26,21 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+            ->subject(Lang::get('Vérifier l\'adresse e-mail'))
+            ->line(Lang::get('Veuillez cliquer sur le bouton ci-dessous pour vérifier votre adresse e-mail.'))
+            ->action(Lang::get('Vérifier l\'adresse e-mail'), $url)
+            ->line(Lang::get('Si vous n\'avez pas créé de compte, aucune action supplémentaire n\'est requise.'));
+        });
+
+        ResetPassword::toMailUsing(function($notifiable, $url){
+            return (new MailMessage)
+            ->subject(Lang::get('Réinitialiser votre mot de passe'))
+            ->line(Lang::get('Vous recevez cet e-mail car nous avons reçu une demande de réinitialisation de mot de passe pour votre compte.'))
+            ->action(Lang::get('Réinitialiser le mot de passe'), $url)
+            ->line(Lang::get('Ce lien de réinitialisation de mot de passe expirera dans :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+            ->line(Lang::get('Si vous n\'avez pas demandé de réinitialisation de mot de passe, aucune action supplémentaire n\'est requise.'));
+        });
     }
 }
