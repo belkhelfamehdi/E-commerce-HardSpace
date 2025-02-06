ed5dc4ae260bfe0e38a386fd429f9c83e909adbc
Belkhelfa Mehdi <mehdibelkhelfa6@gmail.com>
2025-02-06 16:18:14 +0100

    Remove unused .gitignore file and add Livewire component and package cache files

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'add-to-cart' => 'App\\Http\\Livewire\\AddToCart',
+  'cart' => 'App\\Http\\Livewire\\Cart',
+  'cart-list' => 'App\\Http\\Livewire\\CartList',
+  'cart-update' => 'App\\Http\\Livewire\\CartUpdate',
+);
