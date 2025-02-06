069ee14a0dde6c493e668b96caf2fb65eb3a5a31
Belkhelfa Mehdi <mehdibelkhelfa6@gmail.com>
2025-02-06 15:27:39 +0100

    Remove HTTPS scheme enforcement in production and add Content Security Policy meta tag for upgrade-insecure-requests in frontend view

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="shortcut icon" href="{{secure_url('frontend/assets/images/logo/logo_black.png')}}" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
